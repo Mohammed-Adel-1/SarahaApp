f2c3c77a14c2c0d5832e887b76318fa45de0020c
Mohammed Adel <mohamalk42@gmail.com>
2026-03-06 16:29:17 +0200

    fixing bugs

--- src/common/utils/cloudinary.js
+++ src/common/utils/cloudinary.js
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { CLOUDINARY_API_SECRET } from '../../../config/config.service.js';
 
 cloudinary.config({ 
   cloud_name: 'dqj8rz9cf', 
   api_key: '613111898773759', 
-  api_secret: 'v_AdbXqfcYWlA2YQuH1AdWbcUZo'
+  api_secret: CLOUDINARY_API_SECRET
 });
 
 export default cloudinary
